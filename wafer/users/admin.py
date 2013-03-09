4b1c222693e0fe17b28c18bf315c40b75753e8ae
Stefano Rivera <stefano@rivera.za.net>
2013-03-09 23:51:32 +0200

    Hook UserProfile into the User admin page

--- wafer/users/admin.py
+++ wafer/users/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from wafer.users.models import UserProfile
 
 
-admin.site.register(UserProfile)
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
