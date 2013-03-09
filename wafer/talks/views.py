087c622357e7ca000088987d00591b91acc9cf88
Neil <neil@dip.sun.ac.za>
2013-03-09 18:07:38 +0200

    Add talk to the database

--- wafer/talks/views.py
+++ wafer/talks/views.py
@@ -10,7 +10,16 @@ def submit(request):
     if request.method == 'POST':
         form = SubmitTalkForm(request.POST)
         if form.is_valid():
-            # FIXME
+            if not request.user.is_authenticated():
+                # Shouldn't happen, but let's be paranoid
+                return HttpResponseRedirect('/')
+            title = form.cleaned_data['title']
+            abstract = form.cleaned_data['abstract']
+            user = request.user
+            # FIXME: Valid that all the authors exist in the
+            # database
+            talk = Talks.objects.create(title=title,
+                    abstract=abstract, corresponding_author=user)
             return HttpResponseRedirect('/')
     else:
         form = SubmitTalkForm()
