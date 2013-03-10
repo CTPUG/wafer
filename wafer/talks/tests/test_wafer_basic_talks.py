4f3d1e90ec4af618ada415f53ddd9eec42bafb38
Stefano Rivera <stefano@rivera.za.net>
2013-03-10 16:28:59 +0200

    Indent with 4 spaces, not 3

--- wafer/talks/tests/test_wafer_basic_talks.py
+++ wafer/talks/tests/test_wafer_basic_talks.py
@@ -2,13 +2,13 @@
 
 
 def test_add_talk():
-   """Create a user and add a talk to it"""
-   from django.contrib.auth.models import User
-   from wafer.talks.models import Talks
+    """Create a user and add a talk to it"""
+    from django.contrib.auth.models import User
+    from wafer.talks.models import Talks
 
-   user = User.objects.create_user('john', 'best@wafer.test', 'johnpassword')
-   talk = Talks.objects.create(title="This is a test talk",
-         abstract="This should be a long and interesting abstract, but isn't",
-         corresponding_author_id=user.id)
+    user = User.objects.create_user('john', 'best@wafer.test', 'johnpassword')
+    talk = Talks.objects.create(title="This is a test talk",
+          abstract="This should be a long and interesting abstract, but isn't",
+          corresponding_author_id=user.id)
 
-   assert user.contact_talks.count() == 1
+    assert user.contact_talks.count() == 1
