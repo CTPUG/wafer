94124a65b5aa540f9f997dfcdbd856207d011555
Neil <neil@dip.sun.ac.za>
2013-04-09 13:57:58 +0200

    Make requirements optional. Add help text and fix display in admin form

--- wafer/conf_registration/models.py
+++ wafer/conf_registration/models.py
@@ -6,15 +6,24 @@ class ConferenceOptionGroup(models.Model):
     """Used to manage relationships"""
     name = models.CharField(max_length=255)
 
+    def __unicode__(self):
+        return u'%s' % self.name
+
 
 class ConferenceOption(models.Model):
 
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     groups = models.ManyToManyField(
-            ConferenceOptionGroup, related_name='members')
+            ConferenceOptionGroup, related_name='members',
+            help_text='Groups this option belongs to.')
     requirements = models.ManyToManyField(
-            ConferenceOptionGroup, related_name='enables')
+            ConferenceOptionGroup, related_name='enables',
+            help_text='Option groups that this relies on',
+            blank=True)
+
+    def __unicode__(self):
+        return u'%s (%.2f)' % (self.name, self.price)
 
 
 class RegisteredAttendee(models.Model):
