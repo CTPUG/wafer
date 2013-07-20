a67b09b188dee8b92f425e3a6424dc0c55f2c5b0
Simon Cross <hodgestar@gmail.com>
2013-07-20 21:17:11 +0200

    Refresh menus on page changes and fix various bugs exposed by using functionality.

--- wafer/menu.py
+++ wafer/menu.py
@@ -1,3 +1,5 @@
+import copy
+
 from django.core.cache import cache
 from django.conf import settings
 
@@ -18,6 +20,15 @@ def get_cached_menus():
 def clear_menu_cache():
     """Clear the cached version of the menu (if any)."""
     cache.delete(CACHE_KEY)
+
+
+def refresh_menu_cache(**kwargs):
+    """Refresh the menu cache.
+
+    Takes **kwargs to make it easier to use as a Django signal handler.
+    """
+    clear_menu_cache()
+    get_cached_menus()
 
 
 def maybe_obj(str_or_obj):
@@ -45,7 +56,7 @@ def maybe_obj(str_or_obj):
 
 def generate_menu():
     """Generate a new list of menus."""
-    root_menu = Menu(settings.WAFER_MENUS)
+    root_menu = Menu(copy.deepcopy(settings.WAFER_MENUS))
     for dynamic_menu_func in settings.WAFER_DYNAMIC_MENUS:
         dynamic_menu_func = maybe_obj(dynamic_menu_func)
         dynamic_menu_func(root_menu)
@@ -81,19 +92,20 @@ class Menu(object):
         self.items = items
 
     @staticmethod
-    def mk_item(self, name, label, url, sort_key=None):
+    def mk_item(name, label, url, sort_key=None):
         return {"name": name, "label": label, "url": url}
 
     @staticmethod
-    def mk_menu(self, name, label, items, sort_key=None):
+    def mk_menu(name, label, items, sort_key=None):
         return {"name": name, "label": label, "items": items}
 
     def _descend_items(self, parts):
         menu_items = self.items
-        for sub_menu in parts[:-1]:
-            matches = [item for item in menu_items if item["name"] == sub_menu]
+        for sub_menu in parts:
+            matches = [item for item in menu_items
+                       if item["name"] == sub_menu and "items" in item]
             if len(matches) == 1:
-                menu_items = matches[0]
+                menu_items = matches[0]["items"]
             else:
                 raise MenuError("Unable to find sub-menu %r of %r"
                                 % (sub_menu, ".".join(parts)))
--- wafer/pages/models.py
+++ wafer/pages/models.py
@@ -1,8 +1,13 @@
+import logging
+logger = logging.getLogger(__name__)
+
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.db.models.signals import post_save
 
 from wafer.snippets.markdown_field import MarkdownTextField
+from wafer.menu import MenuError, refresh_menu_cache
 
 
 class File(models.Model):
@@ -19,6 +24,10 @@ class Page(models.Model):
     parent = models.ForeignKey('self', null=True, blank=True)
     content = MarkdownTextField(
         help_text=_("Markdown contents for the page."))
+    menu = models.CharField(
+        max_length=255,
+        help_text=_("Dotted name of menu to put this page in."),
+        blank=True, null=True, default=None)
     files = models.ManyToManyField(
         File, related_name="pages", null=True, blank=True,
         help_text=_("Images and other files for use in"
@@ -28,7 +37,7 @@ class Page(models.Model):
         return u'%s' % (self.name,)
 
     def get_absolute_url(self):
-        return reverse('wafer_page', args=(self.pk,))
+        return reverse('wafer_page', args=(self.slug,))
 
     class Model:
         unique_together = (('parent', 'slug'),)
@@ -36,4 +45,13 @@ class Page(models.Model):
 
 def page_menus(root_menu):
     """Add page menus."""
-    pass
+    for page in Page.objects.filter(menu__isnull=False):
+        item_name = "%s.%s" % (page.menu, page.slug)
+        try:
+            root_menu.add_item(item_name, page.name, page.get_absolute_url())
+        except MenuError, e:
+            logger.error("Bad menu item %r for page with slug %r."
+                         % (e, page.slug))
+
+
+post_save.connect(refresh_menu_cache, sender=Page)
