5d8453f18689a67185472b20e7daf287ebbdb3ac
Neil <neil@dip.sun.ac.za>
2013-07-24 14:23:33 +0200

    Fix default redirect to use get_script_prefix

--- wafer/pages/urls.py
+++ wafer/pages/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
+from django.core.urlresolvers import get_script_prefix
 from django.views.generic import RedirectView
 
 urlpatterns = patterns(
     'wafer.pages.views',
-    url('^index(?:\.html)?$', RedirectView.as_view(url='/')),
+    url('^index(?:\.html)?$', RedirectView.as_view(url=get_script_prefix())),
     url(r'^(.*)$', 'slug', name='wafer_page'),
 )
