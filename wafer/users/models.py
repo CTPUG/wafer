c5fe461d5b06839483f2a1289e9426c14027e65f
Simon Cross <hodgestar@gmail.com>
2013-09-11 16:29:46 +0200

    Also protect against empty email addresses.

--- wafer/users/models.py
+++ wafer/users/models.py
@@ -33,7 +33,7 @@ class UserProfile(models.Model):
         return self.user.created.all()
 
     def avatar_url(self, size=96, https=True, default='mm'):
-        if self.user.email is None:
+        if not self.user.email:
             return None
         return libravatar_url(self.user.email, size=size, https=https,
                               default=default)
