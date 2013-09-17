db3f69e40b4bdadaac0d417c384bcc0e67c78c02
Simon Cross <hodgestar@gmail.com>
2013-09-17 18:04:41 +0200

    Correctly order sponsors.

--- wafer/sponsors/views.py
+++ wafer/sponsors/views.py
@@ -7,6 +7,18 @@ class ShowSponsors(ListView):
     template_name = 'wafer.sponsors/sponsors.html'
     model = Sponsor
 
+    def get_queryset(self):
+        packages = SponsorshipPackage.objects.all()
+        package_order = dict((p.pk, i) for i, p in enumerate(packages))
+
+        def sponsor_key(sponsor):
+            order = min(package_order[p.pk] for p in sponsor.packages)
+            return (order, sponsor.name)
+
+        sponsors = list(Sponsor.objects.all())
+        sponsors.sort(key=sponsor_key)
+        return sponsors
+
 
 class ShowPackages(ListView):
     template_name = 'wafer.sponsors/packages.html'
