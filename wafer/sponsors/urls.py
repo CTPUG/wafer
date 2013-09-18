993e90c5f675324ca4c38960d41701f8ee4a2f5d
Simon Cross <hodgestar@gmail.com>
2013-09-19 01:06:56 +0200

    Add individual views for sponsors.

--- wafer/sponsors/urls.py
+++ wafer/sponsors/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, url
 
 from wafer.sponsors.views import (
-    ShowSponsors, ShowPackages)
+    ShowSponsors, SponsorView, ShowPackages)
 
 urlpatterns = patterns(
     '',
     url(r'^$', ShowSponsors.as_view(),
         name='wafer_sponsors'),
+    url(r'^(?P<pk>\d+)/$', SponsorView.as_view(), name='wafer_sponsor'),
     url(r'^packages/$', ShowPackages.as_view(),
         name='wafer_sponsorship_packages'),
+
 )
