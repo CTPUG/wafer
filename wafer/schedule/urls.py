578c4402c6b5196cdd86b9d331d3c04cff013868
Neil <neil@dip.sun.ac.za>
2013-09-23 11:14:40 +0200

    Fix urls

--- wafer/schedule/urls.py
+++ wafer/schedule/urls.py
@@ -4,8 +4,6 @@ from wafer.schedule.views import VenueView, ScheduleView
 
 urlpatterns = patterns(
     '',
-    url(r'^/venue/(?<P<pk>\d+)$', VenueView.as_view(),
-        name='wafer_venue'),
-    url(r'^/schedule$', ScheduleView.as_view(),
-        name='wafer_full_schedule'),
+    url(r'^$', ScheduleView.as_view(), name='wafer_full_schedule'),
+    url(r'^/venue/(?P<pk>\d+)/$', VenueView.as_view(), name='wafer_venue'),
 )
