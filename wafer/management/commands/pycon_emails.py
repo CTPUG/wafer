df16c83587b2d84804c5a45ad6eae9dec06e542a
Neil <neil@dip.sun.ac.za>
2013-09-25 13:57:16 +0200

    Add list speaker email command

--- wafer/management/commands/pycon_emails.py
+++ wafer/management/commands/pycon_emails.py
@@ -19,7 +19,13 @@ class Command(BaseCommand):
         make_option('--waiting', action="store_true", default=False,
                     help='Only list people on the waiting list.'),
         make_option('--allauthors', action="store_true", default=False,
-                    help='List author emails only (for all talks)')
+                    help='List author emails only (for all talks)'),
+        make_option('--speakers', action="store_true", default=False,
+                    help='List speaker email addresses'
+                    ' (for accepted talks)'),
+        make_option('--allspeakers', action="store_true", default=False,
+                    help='List speaker email addresses'
+                    ' (for all talks)'),
     ])
 
     def _attendee_emails(self, options):
@@ -61,9 +67,31 @@ class Command(BaseCommand):
                              ' '.join(titles))]
             csv_file.writerow(row)
 
+    def _speaker_emails(self, options):
+        people = User.objects.filter(talks__isnull=False).distinct()
+
+        csv_file = csv.writer(sys.stdout)
+        for person in people:
+            if options['allspeakers']:
+                titles = [x.title for x in person.talks.all()]
+            else:
+                titles = [x.title for x in
+                          person.talks.filter(status=ACCEPTED)]
+                if not titles:
+                    continue
+            # get_full_name may be blank, since we don't require that
+            # the user specify it, but we will have the email as an
+            # identifier
+            row = [x.encode("utf-8")
+                   for x in (person.get_full_name(), person.email,
+                             ' '.join(titles))]
+            csv_file.writerow(row)
+
     def handle(self, *args, **options):
 
         if options['authors'] or options['allauthors']:
             self._author_emails(options)
+        elif options['speakers'] or options['allspeakers']:
+            self._speaker_emails(options)
         else:
             self._attendee_emails(options)
