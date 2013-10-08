60e3af0cc272a1af3136acec346ac2845c8155d4
Neil <neil@dip.sun.ac.za>
2013-10-08 17:37:14 +0200

    Replace query-string syntax with urlconf approach

--- wafer/users/urls.py
+++ wafer/users/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns(
     '',
     url(r'^$', UsersView.as_view(),
         name='wafer_users'),
+    url(r'^page(?P<page>\d+)$', UsersView.as_view(),
+        name='wafer_users_page'),
     url(r'^(?P<username>[\w.@+-]+)/$', ProfileView.as_view(),
         name='wafer_user_profile'),
     url(r'^(?P<username>[\w.@+-]+)/edit/$', EditUserView.as_view(),
