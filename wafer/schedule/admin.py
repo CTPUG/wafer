a6432995108617702e7a857c8ce0d1c01cd0d1a0
Neil <neil@dip.sun.ac.za>
2013-10-18 18:43:53 +0200

    Restrict list of talks to accepted ones only

--- wafer/schedule/admin.py
+++ wafer/schedule/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
+from django import forms
 
 from wafer.schedule.models import Venue, Slot, ScheduleItem
+from wafer.talks.models import Talk, ACCEPTED
+
+class ScheduleItemAdminForm(forms.ModelForm):
+    class Meta:
+        model = ScheduleItem
+
+    def __init__(self, *args, **kwargs):
+        super(ScheduleItemAdminForm, self).__init__(*args, **kwargs)
+        self.fields['talk'].queryset = Talk.objects.filter(status=ACCEPTED)
+
+
+class ScheduleItemAdmin(admin.ModelAdmin):
+    form = ScheduleItemAdminForm
 
 
 admin.site.register(Slot)
 admin.site.register(Venue)
-admin.site.register(ScheduleItem)
+admin.site.register(ScheduleItem, ScheduleItemAdmin)
