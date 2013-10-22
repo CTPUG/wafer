e69559b81e9b52eb0834df67b0197aa0f734db3c
Neil <neil@dip.sun.ac.za>
2013-10-22 17:07:43 +0200

    Make talk status editale from the talk list overview

--- wafer/talks/admin.py
+++ wafer/talks/admin.py
@@ -5,6 +5,7 @@ from wafer.talks.models import Talk
 
 class TalkAdmin(admin.ModelAdmin):
     list_display = ('corresponding_author', 'title', 'status')
+    list_editable = ('status',)
 
 
 admin.site.register(Talk, TalkAdmin)
