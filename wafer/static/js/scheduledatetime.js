f58e4cb4543bc1f474e7650e3d474ddfdaacee7e
Neil <neil@dip.sun.ac.za>
2014-06-09 15:51:08 +0200

    Somewhat more idiomatic jQuery.
    Add comments explaining some of the constraints.

--- wafer/static/js/scheduledatetime.js
+++ wafer/static/js/scheduledatetime.js
@@ -1,22 +1,31 @@
 // Basic idea from Bojan Mihelac -
 // http://source.mihelac.org/2010/02/19/django-time-widget-custom-time-shortcuts/
 
-var Schedule = {
-   init: function() {
-      time_format = get_format('TIME_INPUT_FORMATS')[0];
-      django.jQuery(".timelist").each(function(num) {
-         // Clear existing list
-         django.jQuery( this ).empty();
-         // Fill list with time values
-         for (i=8; i < 20; i++) {
-            var time = new Date(1970,1,1,i,0,0);
-            quickElement("a", quickElement("li", this, ""),
-               time.strftime(time_format), "href",
-               "javascript:DateTimeShortcuts.handleClockQuicklink(" + num +
-               ", '" + time.strftime(time_format) + "');");
-         }
-      });
-   }
-}
+// Django imports jQuery into the admin site using noConflict(True)
+// We wrap this in an anonymous function to stick to jQuery's $ idiom
+// and ensure we're using the admin version of jQuery, rather than
+// anything else pulled in
 
-addEvent( window, 'load', Schedule.init);
+(function($) {
+   var Schedule = {
+      init: function() {
+         time_format = get_format('TIME_INPUT_FORMATS')[0];
+         $(".timelist").each(function(num) {
+            // Clear existing list
+            $( this ).empty();
+            // Fill list with time values
+            for (i=8; i < 20; i++) {
+               var time = new Date(1970,1,1,i,0,0);
+               quickElement("a", quickElement("li", this, ""),
+                  time.strftime(time_format), "href",
+                  "javascript:DateTimeShortcuts.handleClockQuicklink(" + num +
+                  ", '" + time.strftime(time_format) + "');");
+            }
+         });
+      }
+   }
+
+   // We need to be called after django's DateTimeShortcuts.init, which
+   // is triggered by a load event
+   $( window ).bind('load', Schedule.init);
+}(django.jQuery));
