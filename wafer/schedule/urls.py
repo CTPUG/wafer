576364b432274d2cacdde418335ed7d471eba38d
Neil <neil@dip.sun.ac.za>
2014-08-19 14:13:58 +0200

    Hook up displaying currently showing view

--- wafer/schedule/urls.py
+++ wafer/schedule/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
-from wafer.schedule.views import VenueView, ScheduleView
+from wafer.schedule.views import VenueView, ScheduleView, CurrentView
 
 urlpatterns = patterns(
     '',
     url(r'^$', ScheduleView.as_view(), name='wafer_full_schedule'),
     url(r'^venue/(?P<pk>\d+)/$', VenueView.as_view(), name='wafer_venue'),
+    url(r'^current/$', CurrentView.as_view(), name='wafer_current'),
 )
