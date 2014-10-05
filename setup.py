cdc3fa12052db9dd4cc88300c170282c8c665cbf
Grigi <nagrigoriadis@gmail.com>
2014-10-05 16:02:06 +0200

    setup.py installs different set of deps for py3

--- setup.py
+++ setup.py
@@ -1,4 +1,51 @@
-from setuptools import setup, find_packages
+import sys
+
+from setuptools import find_packages, setup
+
+REQUIRES = [
+    'Django>=1.6',
+    'django-crispy-forms',
+    'django-nose',
+    'django-registration-redux',
+    'pillow',
+    'pyLibravatar',
+    'pytz',
+    'requests',
+    'django-medusa',
+]
+
+SOURCES = []
+
+REQUIRES2 = [
+    'pydns',
+    # markdown 2.5 drops support for Python 2.6. Django 1.7 doesn't
+    # support 2.6 either, so we can drop this restriction
+    # when we move to Django 1.7
+    'markdown<2.5',
+    # markitup 2.2 breaks on python 2 with module level imports, which we
+    # need to use for markdown, so avoid using that
+    'django-markitup<=2.1',
+]
+
+SOURCES2 = []
+
+REQUIRES3 = [
+    'py3dns',
+    'markdown>=2.5',
+    'django-markitup>=2.2',
+]
+
+SOURCES3 = [
+    # Until Pyton3 support is merged to master, use this branch
+    'https://github.com/kezabelle/django-medusa/archive/44d82054d39b794033e5df25fd9e630d05c97f3b.zip#egg=django-medusa-0.2.1',
+]
+
+if sys.version_info < (3, 0):
+    REQUIRES += REQUIRES2
+    SOURCES += SOURCES2
+else:
+    REQUIRES += REQUIRES3
+    SOURCES += SOURCES3
 
 setup(
     name="wafer",
@@ -11,25 +58,8 @@ setup(
     author_email='ctpug@googlegroups.com',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        'Django>=1.6',
-        'django-crispy-forms',
-        'django-nose',
-        'django-registration-redux',
-        'pillow',
-        'pyLibravatar',
-        'pydns',
-        'pytz',
-        'requests',
-        # markdown 2.5 drops support for Python 2.6. Django 1.7 doesn't
-        # support 2.6 either, so we can drop this restriction
-        # when we move to Django 1.7
-        'markdown<2.5',
-        'django-medusa',
-        # markitup 2.2 breaks on python 2 with module level imports, which we
-        # need to use for markdown, so avoid using that
-        'django-markitup<=2.1,>2.2',
-    ],
+    install_requires=REQUIRES,
+    dependency_links=SOURCES,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -37,6 +67,7 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3.3'
         'Framework :: Django',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Internet :: WWW/HTTP',
