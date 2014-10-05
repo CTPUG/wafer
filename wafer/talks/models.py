071589c7adab0f0574eedcb4487a6be01b39407b
Simon Cross <hodgestar@gmail.com>
2014-10-05 11:17:12 +0200

    Add view_all permissions for talks.

--- wafer/talks/models.py
+++ wafer/talks/models.py
@@ -23,6 +23,11 @@ class TalkType(models.Model):
 
 class Talk(models.Model):
 
+    class Meta:
+        permissions = (
+            ("view_all", "Can see all talks"),
+        )
+
     TALK_STATUS = (
         (ACCEPTED, 'Accepted'),
         (REJECTED, 'Not Accepted'),
@@ -101,6 +106,22 @@ class TalkUrl(models.Model):
        Note that these are explicitly not intended to be exposed to the
        user, but exist for use by the conference organisers."""
 
-    description =  models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
     url = models.URLField()
     talk = models.ForeignKey(Talk)
+
+
+if settings.WAFER_NEEDS_SOUTH:
+    # Django 1.7 updates permissions automatically when migrate is run,
+    # but South 1.0 still needs this to be explicitly hooked up like we
+    # do here.
+    from south.signals import post_migrate
+
+    def update_permissions_after_migration(app, **kwargs):
+        from django.db.models import get_app, get_models
+        from django.contrib.auth.management import create_permissions
+
+        create_permissions(get_app(app), get_models(),
+                           verbosity=2 if settings.DEBUG else 0)
+
+    post_migrate.connect(update_permissions_after_migration)
