fb6f9155dd6fd8f377b8df8f16e84fd85f162d70
Grigi <nagrigoriadis@gmail.com>
2014-10-08 06:55:08 +0200

    Updated classifiers to be complete

--- setup.py
+++ setup.py
@@ -66,8 +66,11 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6'
         'Programming Language :: Python :: 2.7'
+        'Programming Language :: Python :: 3'
         'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.4'
         'Framework :: Django',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Internet :: WWW/HTTP',
