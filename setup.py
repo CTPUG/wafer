c7a6c7fefe4711479b96e494921e1b65d235e924
Simon Cross <hodgestar@gmail.com>
2014-10-17 21:03:04 +0200

    Fix syntax of setup.py after broken merge from master.

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ setup(
         # markitup 2.2 breaks on python 2 with module level imports, which we
         # need to use for markdown, so avoid using that
         'django-markitup<=2.1,>2.2',
-    setup_requires = [
+    ],
+    setup_requires=[
         # Add setuptools-git, so we get correct behaviour for
         # include_package_data
         'setuptools_git >= 1.0',
