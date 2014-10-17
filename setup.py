3dbed9b4cd0117b65ad89849b5d9fa764f90d7a3
Simon Cross <hodgestar@gmail.com>
2014-10-17 21:06:18 +0200

    Version 0.1.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wafer",
-    version="0.1.0",
+    version="0.1.1",
     url='http://github.com/CTPUG/wafer',
     license='MIT',
     description="A wafer-thin Django library for running small conferences.",
