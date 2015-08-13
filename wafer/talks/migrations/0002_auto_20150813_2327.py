c699a331ed8976069731f6bc7f61871123810865
Simon Cross <hodgestar@gmail.com>
2015-08-13 23:27:51 +0200

    Add migration for view_all_talks permission.

---
+++ wafer/talks/migrations/0002_auto_20150813_2327.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('talks', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='talk',
+            options={'permissions': (('view_all_talks', 'Can see all talks'),)},
+        ),
+    ]
