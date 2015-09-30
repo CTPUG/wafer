831b49fe3c6250001ffb38c9a22e6ffb2d56a2fc
Neil <neil@dip.sun.ac.za>
2015-09-30 11:44:55 +0200

    Deletion should also invalidate the cachec schedule checks, so hook that up

--- wafer/schedule/models.py
+++ wafer/schedule/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.utils.encoding import python_2_unicode_compatible
 
 from wafer.snippets.markdown_field import MarkdownTextField
@@ -192,3 +192,8 @@ post_save.connect(invalidate_check_schedule, sender=Day)
 post_save.connect(invalidate_check_schedule, sender=Venue)
 post_save.connect(invalidate_check_schedule, sender=Slot)
 post_save.connect(invalidate_check_schedule, sender=ScheduleItem)
+
+post_delete.connect(invalidate_check_schedule, sender=Day)
+post_delete.connect(invalidate_check_schedule, sender=Venue)
+post_delete.connect(invalidate_check_schedule, sender=Slot)
+post_delete.connect(invalidate_check_schedule, sender=ScheduleItem)
