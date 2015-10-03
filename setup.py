b1fb7a0f3329a2eb5165a7c1c521190108adf43c
Neil <neil@dip.sun.ac.za>
2015-10-03 12:59:40 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="wafer",
-    version="0.2.8a",
+    version="0.3.0",
     url='http://github.com/CTPUG/wafer',
     license='MIT',
     description="A wafer-thin Django library for running small conferences.",
