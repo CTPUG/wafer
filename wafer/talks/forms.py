33ba6afd26f3a981a0e75f254c24a78c421684a6
martin f. krafft <madduck@madduck.net>
2015-11-30 04:58:25 +0100

    Add corresp._author to talk edit form, but read-only
    It's gross, but it works. This adds the corresponding author as
    a read-only text field to the talk edit form, in such a way that it
    cannot be abused and we need not check/validate to guard against abuse.
    
    Signed-off-by: martin f. krafft <madduck@madduck.net>

--- wafer/talks/forms.py
+++ wafer/talks/forms.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext as _
 
 from crispy_forms.bootstrap import FormActions
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit, HTML
+from crispy_forms.layout import Submit, HTML, Div, Field
 from markitup.widgets import MarkItUpWidget
 
 from wafer.talks.models import Talk
@@ -25,6 +25,24 @@ class TalkForm(forms.ModelForm):
                             _('Delete')))))
         else:
             self.helper.add_input(submit_button)
+        # insert before authors
+        # TODO: this is *YUK*, but if we add the field to Meta.fields below,
+        # then we can disable the widget, sure, but it'd still load all the 
+        # data, and we'd need to validate that the submitted form isn't trying
+        # to change the data if someone bypasses (removes) the HTML disabled
+        # tag. The following is basically display-only as the input field
+        # has no name.
+        self.helper.layout.insert(TalkForm.Meta.fields.index('authors'),
+                Div(
+                    HTML('<label class="control-label" for="id_corresponding_author">Corresponding author: </label>'),
+                    Div(
+                        HTML('<input id="id_corresponding_author" class="textinput textInput form-control" type="text" value="%s" readonly/>'
+                            % instance.corresponding_author.get_full_name()),
+                        **{'class':'controls'}
+                        ),
+                    **{'class':'form-group','id':'id_corresponding_author'}
+                    )
+                )
 
     class Meta:
         model = Talk
