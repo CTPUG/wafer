60675d5ef570b9de7ebd689cb85dc944778355cd
martin f. krafft <madduck@madduck.net>
2015-12-09 12:52:21 +0100

    Doc strings for ModelAdmin classes
    Signed-off-by: martin f. krafft <madduck@madduck.net>

--- wafer/kvpairs/admin.py
+++ wafer/kvpairs/admin.py
@@ -8,6 +8,7 @@ from wafer.kvpairs.models import Key, KeyValuePair
 
 @admin.register(Key)
 class KeyAdmin(admin.ModelAdmin):
+    '''Admin interface class for the Key model'''
     fields = (('owner', 'group'), 'model_ct', 'name')
     list_display = ('get_fully_qualified_key_name', 'owner', 'group')
     search_fields = ('name',)
@@ -15,6 +16,7 @@ class KeyAdmin(admin.ModelAdmin):
 
 @admin.register(KeyValuePair)
 class KeyValuePairAdmin(admin.ModelAdmin):
+    '''Admin interface class for the KeyValuePair model'''
     fields = ('key', 'ref_id', 'value')
     list_display = ('key', 'get_ref_instance', 'value')
     search_fields = ('value',)
