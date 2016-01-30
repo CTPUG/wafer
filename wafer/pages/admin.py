a3c1822dd2942de4b6bf5cac14039e6789babf85
Neil <neil@dip.sun.ac.za>
2016-01-30 15:26:24 +0200

    Add reversion support to Pages

--- wafer/pages/admin.py
+++ wafer/pages/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 from wafer.pages.models import File, Page
 
+from reversion.admin import VersionAdmin
 
-class PageAdmin(admin.ModelAdmin):
+
+class PageAdmin(VersionAdmin, admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
     list_display = ('name', 'slug', 'get_people_display_names', 'get_in_schedule')
 
