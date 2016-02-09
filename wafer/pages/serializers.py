11ab81f67df3f7579cb1b85d87499480c3cea351
Neil <neil@dip.sun.ac.za>
2016-02-09 17:49:56 +0200

    Add create & update methods for page API

--- wafer/pages/serializers.py
+++ wafer/pages/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from reversion import revisions
 
 from wafer.pages.models import Page
 
@@ -7,7 +8,17 @@ class PageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Page
+        exclude = ('_content_rendered',)
 
+    @revisions.create_revision()
     def create(self, validated_data):
-        # TODO: Implement
+        revisions.set_comment("Created via REST api")
         return super(PageSerializer, self).create(validated_data)
+
+    @revisions.create_revision()
+    def update(self, page, validated_data):
+        revisions.set_comment("Changed via REST api")
+        page.parent = validated_data['parent']
+        page.content = validated_data['content']
+        page.save()
+        return page
