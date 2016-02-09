103efc3a134c238499a7b1c612a66ac9171a00f7
Neil <neil@dip.sun.ac.za>
2016-02-09 18:06:23 +0200

    Fill in talk serializer

--- wafer/talks/serializers.py
+++ wafer/talks/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from reversion import revisions
 
 from wafer.talks.models import Talk
 
@@ -7,7 +8,22 @@ class TalkSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Talk
+        exclude = ('_abstract_rendered', )
 
+
+    @revisions.create_revision()
     def create(self, validated_data):
-        # TODO: Implement
+        revisions.set_comment("Created via REST api")
         return super(TalkSerializer, self).create(validated_data)
+
+    @revisions.create_revision()
+    def update(self, talk, validated_data):
+        revisions.set_comment("Changed via REST api")
+        talk.abstract = validated_data['abstract']
+        talk.title = validated_data['title']
+        talk.status = validated_data['status']
+        talk.talk_type = validated_data['talk_type']
+        talk.notes = validated_data['notes']
+        talk.private_notes = validated_data['private_notes']
+        talk.save()
+        return talk
