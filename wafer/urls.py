1faf7f8335489f12a03a8d5ce3f9545e6a8e6f6f
Stefano Rivera <stefano@rivera.za.net>
2016-03-10 16:35:10 -0800

    There's a helper for this

--- wafer/urls.py
+++ wafer/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, patterns, url
+from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 
@@ -23,8 +24,5 @@ urlpatterns = patterns(
 
 # Serve media
 if settings.DEBUG:
-    urlpatterns += patterns(
-        '',
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
-            {'document_root': settings.MEDIA_ROOT}),
-    )
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
