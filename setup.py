eeaebac45f85550f3ad1d82ea4f4aee8d3f10b1b
Stefano Rivera <stefano@rivera.za.net>
2016-03-15 14:00:47 -0700

    Release 0.3.3

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="wafer",
-    version="0.3.3a",
+    version="0.3.3",
     url='http://github.com/CTPUG/wafer',
     license='ISC',
     description="A wafer-thin Django library for running small conferences.",
