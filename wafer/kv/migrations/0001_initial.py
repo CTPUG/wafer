f6b300f6ca0d70f05b22143665f0a802e9a3dc0c
Neil <neil@dip.sun.ac.za>
2016-03-29 21:22:09 +0200

    postgres isn't happy without this

---
+++ wafer/kv/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import jsonfield.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '0006_require_contenttypes_0002'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='KeyValue',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('key', models.CharField(max_length=64, db_index=True)),
+                ('value', jsonfield.fields.JSONField()),
+                ('group', models.ForeignKey(to='auth.Group')),
+            ],
+        ),
+    ]
