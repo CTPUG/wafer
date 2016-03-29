efb0aa6c68ed9b5ab5c855464dc0b611506326d2
Neil <neil@dip.sun.ac.za>
2016-03-30 00:01:52 +0200

    Tweak kv migration to improve compatibility across Django versions

--- wafer/kv/migrations/0001_initial.py
+++ wafer/kv/migrations/0001_initial.py
@@ -2,13 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from django.conf import settings
 import jsonfield.fields
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0006_require_contenttypes_0002'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL)
     ]
 
     operations = [
