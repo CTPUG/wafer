0f0b13d211099bd59ecff5c2815dab0f0c9d5bde
Neil <neil@dip.sun.ac.za>
2016-04-11 16:10:16 +0200

    Restrict view to only owned key-value pairs

--- wafer/kv/views.py
+++ wafer/kv/views.py
@@ -6,10 +6,14 @@ from wafer.kv.permissions import KeyValueGroupPermission
 
 class KeyValueViewSet(viewsets.ModelViewSet):
     """API endpoint that allows key-value pairs to be viewed or edited."""
-    queryset = KeyValue.objects.all()  # Needed for the REST Permissions
+    queryset = KeyValue.objects.none()  # Needed for the REST Permissions
     serializer_class = KeyValueSerializer
     permission_classes = (KeyValueGroupPermission, )
 
     def get_queryset(self):
-        # Space for future expansion
-        return super(KeyValueViewSet, self).get_queryset()
+        # Restrict the list to only those that match the user's
+        # groups
+        if self.request.user.id is not None:
+            grp_ids = [x.id for x in self.request.user.groups.all()]
+            return KeyValue.objects.filter(group_id__in=grp_ids)
+        return KeyValue.objects.none()
