ec2023825a060b5caf0a34a94ea4192cfb7c1b57
Stefano Rivera <stefano@rivera.za.net>
2016-06-15 00:08:46 +0200

    Remove KV pairs from admin, makes it dog slow

--- wafer/users/admin.py
+++ wafer/users/admin.py
@@ -18,15 +18,6 @@ class UserAdmin(UserAdmin):
     inlines = (UserProfileInline,)
 
 
-# UserProfile-centric (a hack, to customise KV Pairs)
-
-class KVPairsInline(admin.StackedInline):
-    model = UserProfile.kv.through
-    verbose_name_plural = 'Key Value pairs'
-    list_display = ('key', 'value')
-    extra = 1
-
-
 def username(obj):
     return obj.user.username
 
@@ -39,7 +30,6 @@ class UserProfileAdmin(admin.ModelAdmin):
     model = UserProfile
     readonly_fields = ('user',)
     exclude = ('kv',)
-    inlines = (KVPairsInline,)
     list_select_related = ('user',)
     list_display = (username, 'display_name', email)
     search_fields = ('user__username', 'user__first_name', 'user__last_name',
