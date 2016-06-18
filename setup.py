67c15fdf17107dd6f5be92a0201ef89f95b719e8
Stefano Rivera <stefano@rivera.za.net>
2016-06-18 12:49:52 +0200

    Post-release bump

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="wafer",
-    version="0.3.7",
+    version="0.3.8a",
     url='http://github.com/CTPUG/wafer',
     license='ISC',
     description="A wafer-thin Django library for running small conferences.",
