54eac5df0371d317e94e488b2840454d08f9ae65
Neil <neil@dip.sun.ac.za>
2016-06-29 18:38:37 +0200

    Add support for an additional css class based on the talk type in the schedule

--- wafer/schedule/admin.py
+++ wafer/schedule/admin.py
@@ -186,6 +186,7 @@ class ScheduleItemAdmin(admin.ModelAdmin):
     form = ScheduleItemAdminForm
 
     change_list_template = 'admin/scheduleitem_list.html'
+    readonly_fields = ('get_talk_css_class',)
     list_display = ('get_start_time', 'venue', 'get_title', 'expand')
     list_editable = ('expand',)
 
