335ac0ade0f82c0576e05afcd77a09b3289d24be
Neil <neil@dip.sun.ac.za>
2016-08-16 17:00:54 +0200

    Split out useful functions from admin view

--- wafer/compare/admin.py
+++ wafer/compare/admin.py
@@ -47,6 +47,66 @@ class DateModifiedFilter(SimpleListFilter):
         return queryset.filter(pk__in=[x.object_id for x in revisions])
 
 
+
+def get_date(revision):
+    return revision.revision.date_created.strftime("%Y-%m-%d %H:%m:%S")
+
+
+def get_author(revision):
+    if revision.revision.user:
+        return revision.revision.user.username
+    return 'Unknown'
+
+
+def make_diff(current, revision):
+    """Create the difference between the current revision and a previous version"""
+    the_diff = []
+    dmp = diff_match_patch()
+
+    for field in (set(current.field_dict.keys()) | set(revision.field_dict.keys())):
+        # These exclusions really should be configurable
+        if field == 'id' or field.endswith('_rendered'):
+            continue
+        # KeyError's may happen if the database structure changes
+        # between the creation of revisions. This isn't ideal,
+        # but should not be a fatal error.
+        # Log this?
+        missing_field = False
+        try:
+            cur_val = current.field_dict[field] or ""
+        except KeyError:
+            cur_val = "No such field in latest version\n"
+            missing_field = True
+        try:
+            old_val = revision.field_dict[field] or ""
+        except KeyError:
+            old_val = "No such field in old version\n"
+            missing_field = True
+        if missing_field:
+            # Ensure that the complete texts are marked as changed
+            # so new entries containing any of the marker words
+            # don't show up as differences
+            diffs = [(dmp.DIFF_DELETE, old_val), (dmp.DIFF_INSERT, cur_val)]
+            patch =  dmp.diff_prettyHtml(diffs)
+        elif isinstance(cur_val, Markup):
+            # we roll our own diff here, so we can compare of the raw
+            # markdown, rather than the rendered result.
+            if cur_val.raw == old_val.raw:
+                continue
+            diffs = dmp.diff_main(old_val.raw, cur_val.raw)
+            patch =  dmp.diff_prettyHtml(diffs)
+        elif cur_val == old_val:
+            continue
+        else:
+            # Compare the actual field values
+            diffs = dmp.diff_main(force_text(old_val), force_text(cur_val))
+            patch = dmp.diff_prettyHtml(diffs)
+        the_diff.append((field, patch))
+
+    the_diff.sort()
+    return the_diff
+
+
 class CompareVersionAdmin(VersionAdmin):
 
     compare_template = "admin/wafer.compare/compare.html"
@@ -74,55 +134,14 @@ class CompareVersionAdmin(VersionAdmin):
         # The reversion we want to compare to
         current = Version.objects.get_for_object_reference(self.model, object_id)[0]
         revision = Version.objects.get_for_object_reference(self.model, object_id).filter(id=version_id)[0]
-        the_diff = []
-        dmp = diff_match_patch()
 
-        for field in (set(current.field_dict.keys()) | set(revision.field_dict.keys())):
-            # These exclusions really should be configurable
-            if field == 'id' or field.endswith('_rendered'):
-                continue
-            # KeyError's may happen if the database structure changes
-            # between the creation of revisions. This isn't ideal,
-            # but should not be a fatal error.
-            # Log this?
-            missing_field = False
-            try:
-                cur_val = current.field_dict[field] or ""
-            except KeyError:
-                cur_val = "No such field in latest version\n"
-                missing_field = True
-            try:
-                old_val = revision.field_dict[field] or ""
-            except KeyError:
-                old_val = "No such field in old version\n"
-                missing_field = True
-            if missing_field:
-                # Ensure that the complete texts are marked as changed
-                # so new entries containing any of the marker words
-                # don't show up as differences
-                diffs = [(dmp.DIFF_DELETE, old_val), (dmp.DIFF_INSERT, cur_val)]
-                patch =  dmp.diff_prettyHtml(diffs)
-            elif isinstance(cur_val, Markup):
-                # we roll our own diff here, so we can compare of the raw
-                # markdown, rather than the rendered result.
-                if cur_val.raw == old_val.raw:
-                    continue
-                diffs = dmp.diff_main(old_val.raw, cur_val.raw)
-                patch =  dmp.diff_prettyHtml(diffs)
-            elif cur_val == old_val:
-                continue
-            else:
-                # Compare the actual field values
-                diffs = dmp.diff_main(force_text(old_val), force_text(cur_val))
-                patch = dmp.diff_prettyHtml(diffs)
-            the_diff.append((field, patch))
-
-        the_diff.sort()
+        the_diff = make_diff(current, revision)
 
         context = {
             "title": _("Comparing current %(model)s with revision created %(date)s") % {
                 'model': current,
-                'date' : revision.revision.date_created.strftime("%Y-%m-%d %H:%m:%S")},
+                'date' : get_date(revision),
+                },
             "opts": opts,
             "compare_list_url": reverse("%s:%s_%s_comparelist" % (self.admin_site.name, opts.app_label, opts.model_name),
                                                                   args=(quote(object_id),)),
