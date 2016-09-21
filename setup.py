32333d26dbf5ab4d3693bfa76b1240dbda7e685d
Neil <neil@dip.sun.ac.za>
2016-09-21 21:38:39 +0200

    Post release version bump

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="wafer",
-    version="0.4.3",
+    version="0.4.4a",
     url='http://github.com/CTPUG/wafer',
     license='ISC',
     description="A wafer-thin Django library for running small conferences.",
