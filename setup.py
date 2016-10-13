0efd91a7872a7eb600a315c4c23c40ec83f37b49
Neil <neil@dip.sun.ac.za>
2016-10-13 10:52:55 +0200

    0.4.8 release

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="wafer",
-    version="0.4.8a",
+    version="0.4.8",
     url='http://github.com/CTPUG/wafer',
     license='ISC',
     description="A wafer-thin Django library for running small conferences.",
