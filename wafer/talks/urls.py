25b26841363d43569468ea9d6935441f478fa9ec
William Stewart <zoidbergwill@gmail.com>
2016-10-13 12:04:54 +0200

    Somehow I missed the change to DRF-extensions in my conflict when rebasing

--- wafer/talks/urls.py
+++ wafer/talks/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from rest_framework import routers
+from rest_framework_extensions import routers
 
 from wafer.talks.views import (
     Speakers, TalkCreate, TalkDelete, TalkUpdate, TalkView, UsersTalks,
