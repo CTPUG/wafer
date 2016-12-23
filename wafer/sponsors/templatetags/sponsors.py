055fa862e09c1a7c215e26034ad9da5998d90d0f
Neil <neil@dip.sun.ac.za>
2016-12-23 16:11:59 +0200

    Add template tag to look up sponsor images

--- wafer/sponsors/templatetags/sponsors.py
+++ wafer/sponsors/templatetags/sponsors.py
@@ -11,3 +11,17 @@ def sponsors():
         'sponsors': Sponsor.objects.all().order_by('packages'),
         'packages': SponsorshipPackage.objects.all().prefetch_related('files'),
     }
+
+
+# We use assignment_tag for compatibility with Django 1.8
+# Once we drop 1.8 support, we should change this to
+# simple_tag
+@register.assignment_tag(takes_context=True)
+def sponsor_image_url(context, name):
+    """Returns the corresponding url from the sponsors images"""
+    sponsor = context['sponsor']
+    if sponsor.files.filter(name=name).exists():
+        # We avoid worrying about multiple matches by always
+        # returning the first one.
+        return sponsor.files.filter(name=name).first().item.url
+    return ''
