0903ab7245a559bcb6d986c722033b60bf11d65d
Neil <neil@dip.sun.ac.za>
2017-01-25 11:39:31 +0200

    Update to current cripsy forms behaviour.
    The bug in which required duplicating the field name when using wrap was
    fixed in django-crispy-forms 1.2.5, but we never updated our usage to
    match. Our minimum Django requirements mean we will us be running a
    sufficiently recent version of crispy-forms.

--- wafer/users/forms.py
+++ wafer/users/forms.py
@@ -37,9 +37,9 @@ class UserProfileForm(forms.ModelForm):
         username = kwargs['instance'].user.username
         self.helper.form_action = reverse('wafer_user_edit_profile',
                                           args=(username,))
-        self.helper['twitter_handle'].wrap(PrependedText, 'twitter_handle',
+        self.helper['twitter_handle'].wrap(PrependedText,
                                            '@', placeholder=_('handle'))
-        self.helper['github_username'].wrap(PrependedText, 'github_username',
+        self.helper['github_username'].wrap(PrependedText,
                                             '@', placeholder=_('username'))
         self.helper.add_input(Submit('submit', _('Save')))
 
