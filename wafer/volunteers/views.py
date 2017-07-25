ac761f74bf97acf1763aa12a197d9ac682e54806
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-07-25 13:48:43 -0400

    Register user if he tries to look up a volunteer profile

--- wafer/volunteers/views.py
+++ wafer/volunteers/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import DetailView
 from django.views.generic.list import ListView
 
+from wafer.users.views import EditOneselfMixin
 from wafer.volunteers.models import Volunteer, Task
 
 
@@ -14,10 +15,17 @@ class TaskView(DetailView):
     template_name = 'wafer.volunteers/task.html'
 
 
-class VolunteerView(DetailView):
+class VolunteerView(EditOneselfMixin, DetailView):
     model = Volunteer
     slug_field = 'user__username'
     template_name = 'wafer.volunteers/volunteer.html'
+
+    def get_object(self, *args, **kwargs):
+        # TODO Find a better way
+        if self.request.user.is_authenticated():
+            Volunteer.objects.get_or_create(user=self.request.user)
+
+        return super(VolunteerView, self).get_object(*args, **kwargs)
 
     def get_context_data(self, **kwargs):
         context = super(VolunteerView, self).get_context_data(**kwargs)
