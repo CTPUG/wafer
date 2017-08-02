6e874375ee1d371a3e6ecb786ade4e1b16d84da5
Simon Cross <hodgestar@gmail.com>
2017-08-02 22:20:18 +0200

    Order paginated KV API results.

--- wafer/kv/views.py
+++ wafer/kv/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets
 from wafer.kv.models import KeyValue
 from wafer.kv.serializers import KeyValueSerializer
 from wafer.kv.permissions import KeyValueGroupPermission
+from wafer.utils import order_results_by
+
 
 class KeyValueViewSet(viewsets.ModelViewSet):
     """API endpoint that allows key-value pairs to be viewed or edited."""
@@ -10,6 +12,7 @@ class KeyValueViewSet(viewsets.ModelViewSet):
     serializer_class = KeyValueSerializer
     permission_classes = (KeyValueGroupPermission, )
 
+    @order_results_by('key', 'id')
     def get_queryset(self):
         # Restrict the list to only those that match the user's
         # groups
