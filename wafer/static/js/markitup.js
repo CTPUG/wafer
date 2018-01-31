ae9d22ebf61d858cb78b32591bafa4872eb2d83f
Stefano Rivera <stefano@rivera.za.net>
2018-01-31 15:23:33 +0100

    data-auto-preview will be a string

--- wafer/static/js/markitup.js
+++ wafer/static/js/markitup.js
@@ -2,7 +2,7 @@
 $('.django-markitup-config').each(function(index) {
   var element = $(this.getAttribute('data-element'));
   var preview_url = this.getAttribute('data-preview-url');
-  var auto_preview = this.getAttribute('data-auto-preview');
+  var auto_preview = this.getAttribute('data-auto-preview') == '1';
   if (!element.hasClass("markItUpEditor")) {
     if (preview_url) {
       mySettings["previewParserPath"] = preview_url;
