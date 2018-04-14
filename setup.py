8c53970a8bf40ed0c1915cc1c83d439bf82fd059
Stefano Rivera <stefano@rivera.za.net>
2018-04-14 17:09:02 -0300

    Release as 0.7.0

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name="wafer",
-    version="0.6.1a",
+    version="0.7.0",
     url='http://github.com/CTPUG/wafer',
     license='ISC',
     description="A wafer-thin Django library for running small conferences.",
--- wafer/__init__.py
+++ wafer/__init__.py
@@ -1,3 +1,3 @@
 """Wafer, a light-weight conference management library for Django."""
 
-__version__ = "0.6.1a"
+__version__ = "0.7.0"
