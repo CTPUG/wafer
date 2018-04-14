415ad882595601ed237a9fed49c4c939c85f1b27
Stefano Rivera <stefano@rivera.za.net>
2018-04-14 12:24:39 -0300

    Use an inclusion tag for reviewed badges
    So that they can be overridden

--- wafer/talks/templatetags/review.py
+++ wafer/talks/templatetags/review.py
@@ -1,23 +1,21 @@
 from django import template
-from django.utils.html import format_html
-from django.utils.translation import ugettext as _
 
 register = template.Library()
 
 
-@register.simple_tag()
+@register.inclusion_tag('wafer.talks/reviewed-badge.html')
 def reviewed_badge(user, talk):
     """Returns a badge for the user's reviews of the talk"""
-    if user.is_anonymous():
-        return ''
-    review = talk.reviews.filter(reviewer=user).first()
-    if not review:
-        return ''
-    if review.is_current():
-        return format_html(
-            '<span class="badge badge-success" title="{}">R</span>',
-            _('Reviewed'))
-    else:
-        return format_html(
-            '<span class="badge badge-info" title="{}">O</span>',
-            _('Reviewed, but outdated (modified since last review)'))
+    context = {
+        'reviewed': False,
+    }
+
+    review = None
+    if not user.is_anonymous():
+        review = talk.reviews.filter(reviewer=user).first()
+
+    if review:
+        context['reviewed'] = True
+        context['review_is_current'] = review.is_current()
+
+    return context
