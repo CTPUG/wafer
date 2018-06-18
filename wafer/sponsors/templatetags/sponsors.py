398a196a9885e38b1bb9ceeae02be109f798ad0f
Neil Muller <drnlmuller@gmail.com>
2018-06-18 21:58:09 +0200

    Improve tagged file approach.
    * Replace dict idea with templatetag
    * Add sponsor footer template tag to show how that would work
    * Change existing blocks to use tagged image lookups everywhere.

--- wafer/sponsors/templatetags/sponsors.py
+++ wafer/sponsors/templatetags/sponsors.py
@@ -12,6 +12,14 @@ def sponsors():
         'packages': SponsorshipPackage.objects.all().prefetch_related('files'),
     }
 
+@register.inclusion_tag('wafer.sponsors/sponsors_footer.html')
+def sponsors_footer():
+    return {
+        'sponsors': Sponsor.objects.all().order_by('packages', 'order', 'id'),
+        'packages': SponsorshipPackage.objects.all().prefetch_related('files'),
+    }
+
+
 
 @register.simple_tag()
 def sponsor_image_url(sponsor, name):
@@ -20,4 +28,12 @@ def sponsor_image_url(sponsor, name):
         # We avoid worrying about multiple matches by always
         # returning the first one.
         return sponsor.files.filter(name=name).first().item.url
+    return ''
+
+
+@register.simple_tag()
+def sponsor_tagged_image(sponsor, tag):
+    """returns the corresponding url from the tagged image list."""
+    if sponsor.files.filter(tag_name=tag).exists():
+        return sponsor.files.filter(tag_name=tag).first().tagged_file.item.url
     return ''
