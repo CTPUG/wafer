851e1bb303775a177394a96ca59333ba50befd2a
Neil Muller <drnlmuller@gmail.com>
2018-11-03 22:11:06 +0200

    Add data migration

--- wafer/schedule/migrations/0009_migrate_to_datetimes.py
+++ wafer/schedule/migrations/0009_migrate_to_datetimes.py
@@ -6,9 +6,86 @@
 # We force the timezone to utc for this conversion.
 from __future__ import unicode_literals
 
-from django.db import migrations, models
-import django.db.models.deletion
-import wafer.snippets.markdown_field
+import datetime
+
+from django.db import migrations
+from django.utils import timezone
+
+def convert_day_to_schedule_block(apps, schema_editor):
+    """For each Day, create a schedule block with the 
+       time block running from 0:00 to 23:59:59."""
+    Day = apps.get_model('schedule', 'Day')
+    ScheduleBlock = apps.get_model('schedule', 'ScheduleBlock')
+    for day in Day.objects.all():
+        start_time = datetime.datetime(year=day.date.year,
+                                       month=day.date.month,
+                                       day=day.date.day,
+                                       hour=0,
+                                       minute=0,
+                                       tzinfo=timezone.utc)
+        end_time = datetime.datetime(year=day.date.year,
+                                       month=day.date.month,
+                                       day=day.date.day,
+                                       hour=23,
+                                       minute=59,
+                                       second=59,
+                                       tzinfo=timezone.utc)
+        block = ScheduleBlock.objects.create(start_time=start_time,
+                                             end_time=end_time)
+        block.save()
+
+def get_day(slot):
+    """reimplement removed get_day helper method for migration"""
+    if slot.day:
+        return slot.day
+    else:
+        return get_day(slot.previous_slot)
+
+
+def convert_slot_time_to_date_time(apps, schema_editor):
+    """For each slot, convert the start and end times to datetimes"""
+    Slot = apps.get_model('schedule', 'Slot')
+    ScheduleBlock = apps.get_model('schedule', 'ScheduleBlock')
+    for slot in Slot.objects.all():
+        day = get_day(slot)
+        if slot.old_start_time:
+            new_start_time = datetime.datetime(year=day.date.year,
+                                               month=day.date.month,
+                                               day=day.date.day,
+                                               hour=slot.old_start_time.hour,
+                                               minute=slot.old_start_time.minute,
+                                               second=slot.old_start_time.second,
+                                               tzinfo=timezone.utc)
+            slot.start_time = new_start_time
+        new_end_time = datetime.datetime(year=day.date.year,
+                                         month=day.date.month,
+                                         day=day.date.day,
+                                         hour=slot.old_end_time.hour,
+                                         minute=slot.old_end_time.minute,
+                                         second=slot.old_end_time.second,
+                                         tzinfo=timezone.utc)
+        slot.end_time = new_end_time
+        slot.save()
+
+
+def add_blocks_to_venues(apps, schema_editor):
+    """For each slot and venue, add the ScheduleBlock corresponding to
+       the existing day."""
+    Venue = apps.get_model('schedule', 'Venue')
+    ScheduleBlock = apps.get_model('schedule', 'ScheduleBlock')
+    for venue in Venue.objects.all():
+        for day in venue.days.all():
+            start_time = datetime.datetime(year=day.date.year,
+                                           month=day.date.month,
+                                           day=day.date.day,
+                                           hour=0,
+                                           minute=0,
+                                           tzinfo=timezone.utc)
+            # Our earlier migration ensures this block exists
+            block = ScheduleBlock.objects.filter(
+                start_time__exact=start_time).first()
+            venue.blocks.add(block)
+        venue.save()
 
 
 class Migration(migrations.Migration):
@@ -18,58 +95,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-    #    migrations.CreateModel(
-    #        name='ScheduleBlock',
-    #        fields=[
-    #            ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-    #            ('start_time', models.DateTimeField(blank=True, null=True)),
-    #            ('end_time', models.DateTimeField(blank=True, null=True)),
-    #        ],
-    #        options={
-    #            'ordering': ['start_time'],
-    #        },
-    #    ),
-    #    migrations.AlterModelOptions(
-    #        name='slot',
-    #        options={'ordering': ['end_time', 'start_time']},
-    #    ),
-    #    migrations.RemoveField(
-    #        model_name='slot',
-    #        name='day',
-    #    ),
-    #    migrations.RemoveField(
-    #        model_name='venue',
-    #        name='days',
-    #    ),
-    #    migrations.RemoveField(
-    #        model_name='slot',
-    #        name='start_time',
-    #    ),
-    #    migrations.RemoveField(
-    #        model_name='slot',
-    #        name='end_time',
-    #    ),
-    #    migrations.AddField(
-    #        model_name='slot',
-    #        name='end_time',
-    #        field=models.DateTimeField(help_text='Slot end time', null=True),
-    #    ),
-    #    migrations.AddField(
-    #        model_name='slot',
-    #        name='start_time',
-    #        field=models.DateTimeField(blank=True, help_text='Start time (if no previous slot selected)', null=True),
-    #    ),
-    #    migrations.AlterField(
-    #        model_name='slot',
-    #        name='previous_slot',
-    #        field=models.ForeignKey(blank=True, help_text='Previous slot if applicable (slots should have either a previous slot OR a day and start time set)', null=True, on_delete=django.db.models.deletion.CASCADE, to='schedule.Slot'),
-    #    ),
-    #    migrations.DeleteModel(
-    #        name='Day',
-    #    ),
-    #    migrations.AddField(
-    #        model_name='venue',
-    #        name='blocks',
-    #        field=models.ManyToManyField(help_text='Blocks (days) on which this venue will be used.', to='schedule.ScheduleBlock'),
-    #    ),
+            migrations.RunPython(convert_day_to_schedule_block),
+            migrations.RunPython(convert_slot_time_to_date_time),
+            migrations.RunPython(add_blocks_to_venues),
     ]
