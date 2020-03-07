2de5b50ddc4481f319e869590efdf1f9aa51ff86
Neil Muller <drnlmuller@gmail.com>
2020-03-07 16:51:19 +0200

    Bump version number for next release

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ def compile_translations():
 
 setup(
     name="wafer",
-    version="0.8.1a",
+    version="0.8.1",
     url='http://github.com/CTPUG/wafer',
     license='ISC',
     description="A wafer-thin Django library for running small conferences.",
