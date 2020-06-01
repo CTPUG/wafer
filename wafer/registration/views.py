6e6113d9534c3d10c81f90e27ea43e3249763742
Stefano Rivera <stefano@rivera.za.net>
2020-05-31 18:56:54 -0700

    Stop using the Django CSRF token in the OAuth handshake
    In 1.10, Django started to modify the CSRF token frequently to counter
    BREACH. Simple string comparisons can no longer be used to validate the
    token, and the token verification API isn't public.
    
    https://code.djangoproject.com/ticket/20869
    https://github.com/django/django/commit/5112e65ef2df1dbb95ff83026b6a962fb2688661
    
    So, just use our own randomly generated secret, stored in the session.
    It's only valid for a single handshake, and rotated on the next login
    attempt.

--- wafer/registration/views.py
+++ wafer/registration/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login
 from django.contrib.auth.views import redirect_to_login
 from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
+from django.utils.crypto import constant_time_compare, get_random_string
 from django.utils.http import urlencode
 
 from wafer.registration.sso import SSOError, debian_sso, github_sso
@@ -25,17 +26,20 @@ def github_login(request):
         raise Http404()
 
     if 'code' not in request.GET:
+        oauth_state = get_random_string(length=32)
+        request.session['oauth_state'] = oauth_state
         return HttpResponseRedirect(
             'https://github.com/login/oauth/authorize?' + urlencode({
                 'client_id': settings.WAFER_GITHUB_CLIENT_ID,
                 'redirect_uri': request.build_absolute_uri(
                     reverse(github_login)),
                 'scope': 'user:email',
-                'state': request.META['CSRF_COOKIE'],
+                'state': oauth_state,
             }))
 
     try:
-        if request.GET['state'] != request.META['CSRF_COOKIE']:
+        oauth_state = request.session.pop('oauth_state', '')
+        if not constant_time_compare(request.GET['state'], oauth_state):
             raise SSOError('Incorrect state')
 
         user = github_sso(request.GET['code'])
