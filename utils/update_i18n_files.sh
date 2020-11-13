236d895f159300f84c0b1d5f3d619605da710a73
Neil Muller <drnlmuller@gmail.com>
2020-11-13 20:23:24 +0200

    Don't hard-code the list of languages
    It's much better to run the script for all languages present, rather
    than keeping a separate list.

--- utils/update_i18n_files.sh
+++ utils/update_i18n_files.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # Update the langauge files and main django.pot file
-LANGS="pt_BR ru"
 
 base=$(dirname $0)
 cd $base/..
 
-for lang in $LANGS; do
+for cand in wafer/locale/*; do
+   if [ ! -d $cand ]; then
+      continue
+   fi
+   lang=$(basename $cand)
    ./manage.py makemessages -l $lang
 done
 ./manage.py makemessages --keep-pot
