6f564442b94a9c22bf978a4b4c654ac4109b6799
Stefano Rivera <stefano@rivera.za.net>
2021-06-02 15:43:13 -0400

    Ban usernames starting with a . (hidden files)

--- wafer/registration/validators.py
+++ wafer/registration/validators.py
@@ -1,12 +1,9 @@
-import re
-
 from django.core.exceptions import ValidationError
 
 
 def validate_username(username):
-    """. and .. lead to problematic URLs and static file names"""
-    if re.match(r'^\.+$', username):
-        raise ValidationError(
-            "usernames cannot consist of only .")
+    """Some usernames lead to problematic URLs and static file names"""
+    if username.startswith('.'):
+        raise ValidationError('usernames cannot start with a "."')
     if username in ('index.html', 'page'):
         raise ValidationError('Not an acceptable username')
