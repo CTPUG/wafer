37af48883f8dac518b7ae1042375eea9b3d625f1
Stefano Rivera <stefano@rivera.za.net>
2021-06-07 10:17:29 -0400

    Post-release version bump

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def compile_translations():
 
 setup(
     name="wafer",
-    version="0.10.0",
+    version="0.10.1a",
     url='http://github.com/CTPUG/wafer',
     license='ISC',
     description="A wafer-thin Django library for running small conferences.",
