957bad34eeb204e6c3d4ceb712fcf472861e3f6a
Stefano Rivera <stefano@rivera.za.net>
2021-07-23 09:37:41 -0400

    Move imports to module-level

--- wafer/talks/tests/test_wafer_basic_talks.py
+++ wafer/talks/tests/test_wafer_basic_talks.py
@@ -1,11 +1,13 @@
 # This tests the very basic talk stuff, to ensure some levels of sanity
 
+from django.contrib.auth import get_user_model
+from django.utils.timezone import datetime, now, utc
+
+from wafer.talks.models import Talk, TalkType
+
 
 def test_add_talk():
     """Create a user and add a talk to it"""
-    from django.contrib.auth import get_user_model
-    from wafer.talks.models import Talk
-
     user = get_user_model().objects.create_user('john', 'best@wafer.test',
                                                 'johnpassword')
     Talk.objects.create(
@@ -18,8 +20,6 @@ def test_add_talk():
 
 def test_filter_talk():
     """Create a second user and check some filters"""
-    from django.contrib.auth import get_user_model
-
     UserModel = get_user_model()
 
     UserModel.objects.create_user('james', 'best@wafer.test', 'johnpassword')
@@ -30,9 +30,6 @@ def test_filter_talk():
 
 def test_multiple_talks():
     """Add more talks"""
-    from wafer.talks.models import Talk
-    from django.contrib.auth import get_user_model
-
     UserModel = get_user_model()
 
     user1 = UserModel.objects.filter(username='john').get()
@@ -56,9 +53,6 @@ def test_multiple_talks():
 
 def test_corresponding_author_details():
     """Create a second user and check some filters"""
-    from django.contrib.auth import get_user_model
-    from wafer.talks.models import Talk
-
     UserModel = get_user_model()
 
     user = UserModel.objects.create_user('jeff', 'best@wafer.test',
@@ -92,15 +86,10 @@ def test_corresponding_author_details():
 
 
 def test_is_late_submission_no_talk_type():
-    from wafer.talks.models import Talk
-
     assert not Talk().is_late_submission
 
 
 def test_is_late_submission_no_deadline():
-    from django.utils.timezone import now
-    from wafer.talks.models import Talk, TalkType
-
     talk_type = TalkType()
     talk = Talk(submission_time=now(), talk_type=talk_type)
 
@@ -108,9 +97,6 @@ def test_is_late_submission_no_deadline():
 
 
 def test_is_late_submission_not_late():
-    from django.utils.timezone import datetime, utc
-    from wafer.talks.models import Talk, TalkType
-
     deadline = datetime(2019, 11, 1, 0, 0, 0, 0, utc)
     before_deadline = datetime(2019, 10, 15, 0, 0, 0, 0, utc)
 
@@ -120,9 +106,6 @@ def test_is_late_submission_not_late():
 
 
 def test_is_late_submission_late():
-    from django.utils.timezone import datetime, utc
-    from wafer.talks.models import Talk, TalkType
-
     deadline = datetime(2019, 11, 1, 0, 0, 0, 0, utc)
     after_deadline = datetime(2019, 11, 2, 0, 0, 0, 0, utc)
 
