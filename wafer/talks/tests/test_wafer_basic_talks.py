356aacf56a689fc364a19d4e4c5ccec61947a624
Stefano Rivera <stefano@rivera.za.net>
2021-07-23 11:36:30 -0400

    Use wafer.tests.utils.create_user in wafer.talks.tests

--- wafer/talks/tests/test_wafer_basic_talks.py
+++ wafer/talks/tests/test_wafer_basic_talks.py
@@ -4,12 +4,12 @@ from django.contrib.auth import get_user_model
 from django.utils.timezone import datetime, now, utc
 
 from wafer.talks.models import Talk, TalkType
+from wafer.tests.utils import create_user
 
 
 def test_add_talk():
     """Create a user and add a talk to it"""
-    user = get_user_model().objects.create_user('john', 'best@wafer.test',
-                                                'johnpassword')
+    user = create_user('john')
     Talk.objects.create(
         title="This is a test talk",
         abstract="This should be a long and interesting abstract, but isn't",
@@ -20,10 +20,9 @@ def test_add_talk():
 
 def test_filter_talk():
     """Create a second user and check some filters"""
+    create_user('james')
+
     UserModel = get_user_model()
-
-    UserModel.objects.create_user('james', 'best@wafer.test', 'johnpassword')
-
     assert UserModel.objects.filter(contact_talks__isnull=False).count() == 1
     assert UserModel.objects.filter(contact_talks__isnull=True).count() == 1
 
@@ -53,16 +52,12 @@ def test_multiple_talks():
 
 def test_corresponding_author_details():
     """Create a second user and check some filters"""
-    UserModel = get_user_model()
-
-    user = UserModel.objects.create_user('jeff', 'best@wafer.test',
-                                         'johnpassword')
+    user = create_user('jeff')
     profile = user.userprofile
     profile.contact_number = '77776'
     profile.save()
 
-    speaker = UserModel.objects.create_user('bob', 'bob@wafer.test',
-                                            'bobpassword')
+    speaker = create_user('bob')
 
     Talk.objects.create(
         title="This is a another test talk",
@@ -75,7 +70,7 @@ def test_corresponding_author_details():
     talk.save()
 
     assert talk.get_authors_display_name() == 'jeff & bob'
-    assert talk.get_corresponding_author_contact() == 'best@wafer.test - 77776'
+    assert talk.get_corresponding_author_contact() == 'jeff@example.com - 77776'
     assert talk.get_corresponding_author_name() == 'jeff (jeff)'
 
     speaker.first_name = 'Bob'
