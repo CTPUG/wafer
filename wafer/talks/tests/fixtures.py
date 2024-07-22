56b932d8d71310a27c755d84eca3c96478bf2150
Neil Muller <drnlmuller@gmail.com>
2024-07-22 15:49:47 +0200

    Add language and track to create_talk

--- wafer/talks/tests/fixtures.py
+++ wafer/talks/tests/fixtures.py
@@ -7,7 +7,8 @@ def create_talk_type(name, **kwargs):
     return TalkType.objects.create(name=name, **kwargs)
 
 
-def create_talk(title, status, username=None, user=None, talk_type=None):
+def create_talk(title, status, username=None, user=None, talk_type=None,
+                talk_track=None, language=None):
     if sum((user is None, username is None)) != 1:
         raise ValueError('One of user OR username must be specified')
     if username:
@@ -20,5 +21,11 @@ def create_talk(title, status, username=None, user=None, talk_type=None):
     talk.save()
     if talk_type:
         talk.talk_type = talk_type
+        talk.save()
+    if talk_track:
+        talk.track = talk_track
+        talk.save()
+    if language:
+        talk.language = language
         talk.save()
     return talk
