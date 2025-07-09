b261fd4fe4e3c6ebf3125a737c203253f8d0e1aa
Neil Muller <drnlmuller@gmail.com>
2025-07-09 19:22:25 +0200

    Add minial tests for review api

--- wafer/talks/tests/test_review_views.py
+++ wafer/talks/tests/test_review_views.py
@@ -66,3 +66,77 @@ class ReviewFormTests(TestCase):
         version = Version.objects.get_for_object(review).order_by('-pk').first()
         self.assertEqual(str(review), version.object_repr)
         self.assertNotEqual(str(review), review_1_str)
+
+
+class ReviewViewSetTests(TestCase):
+
+    def setUp(self):
+        self.super = create_user('super', superuser=True)
+        self.reviewer_a = create_user('reviewer_a', perms=('add_review',))
+        self.reviewer_b = create_user('reviewer_b', perms=['add_review', 'view_all_reviews'])
+        self.aspect_1 = ReviewAspect.objects.create(name='General')
+        self.aspect_2 = ReviewAspect.objects.create(name='Other')
+        self.client = Client()
+
+    def mk_result(self, review_id, talk, reviewer, scores):
+        def mk_aspects(scores):
+            return [
+                {'aspect': {'name': self.aspect_1.name}, 'value': scores[0]},
+                {'aspect': {'name': self.aspect_2.name}, 'value': scores[1]},
+            ]
+        return {
+            'id': review_id, 'talk': talk.talk_id,
+            'reviewer': reviewer.id,
+            'notes': f'<p>{reviewer.username} notes</p>',
+            'scores':  mk_aspects(scores),
+            'avg_score': sum(scores)/len(scores),
+        }
+
+    def test_list_talk_reviews(self):
+        talk = create_talk("Talk", UNDER_CONSIDERATION, "author")
+        talk2 = create_talk("Talk 2", UNDER_CONSIDERATION, "author 2")
+
+        self.client.login(username='reviewer_a', password='reviewer_a_password')
+        response = self.client.post(reverse('wafer_talk_review',  kwargs={'pk': talk.pk}),
+                                    data={'notes': 'reviewer_a notes',
+                                          make_aspect_key(self.aspect_1): '1',
+                                          make_aspect_key(self.aspect_2): '2'})
+        self.client.login(username='reviewer_b', password='reviewer_b_password')
+        response = self.client.post(reverse('wafer_talk_review',  kwargs={'pk': talk.pk}),
+                                    data={'notes': 'reviewer_b notes',
+                                          make_aspect_key(self.aspect_1): '2',
+                                          make_aspect_key(self.aspect_2): '2'})
+
+        response = self.client.post(reverse('wafer_talk_review',  kwargs={'pk': talk2.pk}),
+                                    data={'notes': 'reviewer_b notes',
+                                          make_aspect_key(self.aspect_1): '1',
+                                          make_aspect_key(self.aspect_2): '1'})
+
+
+        # Check that the user with 'view_all_reviews' sees the reviews
+        response = self.client.get('/talks/api/talks/%d/reviews/' % talk.talk_id)
+        self.assertEqual(response.data['results'], [
+            self.mk_result(1, talk, self.reviewer_a, [1, 2]),
+            self.mk_result(2, talk, self.reviewer_b, [2, 2]),
+        ])
+
+        response = self.client.get('/talks/api/talks/%d/reviews/' % talk2.talk_id)
+        self.assertEqual(response.data['results'], [
+            self.mk_result(3, talk2, self.reviewer_b, [1, 1]),
+        ])
+        # Check that the user without the permissions doesn't
+        self.client.login(username='reviewer_a', password='reviewer_a_password')
+        response = self.client.get('/talks/api/talks/%d/reviews/' % talk.talk_id)
+        self.assertEqual(response.data['results'], [])
+        # Check the super user can see the eiews
+        self.client.login(username='super', password='super_password')
+        response = self.client.get('/talks/api/talks/%d/reviews/' % talk.talk_id)
+        self.assertEqual(response.data['results'], [
+            self.mk_result(1, talk, self.reviewer_a, [1, 2]),
+            self.mk_result(2, talk, self.reviewer_b, [2, 2]),
+        ])
+
+        response = self.client.get('/talks/api/talks/%d/reviews/' % talk2.talk_id)
+        self.assertEqual(response.data['results'], [
+            self.mk_result(3, talk2, self.reviewer_b, [1, 1]),
+        ])
